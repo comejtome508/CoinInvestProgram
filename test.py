83c04192aa302ba54d1da4391b753b8d10722c6e
comejtome508 <youngnoh.goh@gmail.com>
2022-02-20 10:38:42 +0900

    Set initial react environments

--- test.py
+++ test.py
@@ -2,8 +2,8 @@
 import pyupbit
 
 
-access = ""          # 본인 값으로 변경
-secret = ""          # 본인 값으로 변경
+access = "QUdwbNBObPKo9UHAiJnxUsGOxSEQ7Fxa4DRlmFCj"          # 본인 값으로 변경
+secret = "Y4ajtQjpWpVyUJCoxTL2zi5VAGxarEJBF3Un3a7P"          # 본인 값으로 변경
 upbit = pyupbit.Upbit(access, secret)
 
 
