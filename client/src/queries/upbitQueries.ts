4cdcaea043a5775b80de109b48886d20337c8a9c
comejtome508 <youngnoh.goh@gmail.com>
2022-03-25 08:45:43 +0900

    주문 조회 쿼리 코드 추가

--- client/src/queries/upbitQueries.ts
+++ client/src/queries/upbitQueries.ts
@@ -4,7 +4,8 @@ import jwt from 'jsonwebtoken';
 import { v4 as uuidv4 } from 'uuid';
 
 import crypto from 'crypto';
-import querystring from "querystring";
+// import querystring from "querystring";
+const queryEncode = require("querystring").encode
 
 const { sign, verify } = jwt;
 
@@ -13,12 +14,35 @@ const access_key = process.env.REACT_APP_UPBIT_ACCESS_KEY
 const secret_key = process.env.REACT_APP_UPBIT_SECRET_KEY
 const server_url = process.env.REACT_APP_UPBIT_SERVER_URL
 
+const state = 'done'
+const uuids = ['']
+
+const non_array_body = {
+    state: state,
+}
+const array_body = {
+    uuids: uuids,
+}
+const body = {
+    ...non_array_body,
+    ...array_body
+}
+
+const uuid_query = uuids.map(uuid => `uuids[]=${uuid}`).join('&')
+const query = queryEncode(non_array_body) + '&' + uuid_query
+
+const hash = crypto.createHash('sha512')
+const queryHash = hash.update(query, 'utf-8').digest('hex')
+
 const payload = {
     access_key: access_key,
     nonce: uuidv4(),
+    query_hash: queryHash,
+    query_hash_alg: 'SHA512',
 }
 
 const token = sign(payload, secret_key!);
+
 
 const getAccountsApi = () => {
     return axios.get("https://cors-anywhere.herokuapp.com/"+server_url+"/v1/accounts", {headers: {Authorization: `Bearer ${token}`}})
@@ -26,6 +50,10 @@ const getAccountsApi = () => {
 
 const upbitApi = () => {
     return axios.get( "https://cors-anywhere.herokuapp.com/"+server_url+'/v1/market/all?isDetails=false')
+}
+
+const getOrderListApi = () => {
+    return axios.get("https://cors-anywhere.herokuapp.com/"+server_url+'/v1/orders?'+query, {headers: {Authorization: `Bearer ${token}`}, data: body})
 }
 
 export const useGetAccounts = () => {
@@ -49,6 +77,25 @@ export const useGetAccounts = () => {
 }
 
 export const useGetAllMarketCode = () => {
+    const onSuccess = (data:any) => {
+        console.log('==========호출성공==========',data);
+    }
+
+    const onError = (error:any) => {
+        console.log('==========호출실패==========',error);
+    } 
+
+    return useQuery('allMarkets', upbitApi, {
+        onSuccess,
+        onError,
+        select: (data) => {
+            console.log(data.data);
+            return data;
+        }
+    })
+}
+
+export const useGetOrderList = () => {
     const onSuccess = (data:any) => {
         console.log('==========호출성공==========',data);
     }
