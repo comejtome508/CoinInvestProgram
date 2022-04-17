2a404c03f4f4dc06a2b5ee0fa05c325da59b4c14
comejtome508 <youngnoh.goh@gmail.com>
2022-04-17 14:54:28 +0900

    [server] cors 라이브러리 추가

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const postRouter = require('./routes/post');
 const userRouter = require('./routes/user');
+const cors = require('cors');
 const db = require('./models');
 const app = express();
 
@@ -9,7 +10,13 @@ db.sequelize.sync()
         console.log('db 연결 성공');
     })
     .catch(console.error);
+//cors 라이브러리로 처리
+app.use(cors({
+    //origin: '' -> 요청 링크 추가}
+    origin: '*',
+    credentials: false,
 
+}));
 // 프론트에서 받은 json 데이터를 서버 route 파일에서 req로 받을 수 있게 변환시켜줌
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
