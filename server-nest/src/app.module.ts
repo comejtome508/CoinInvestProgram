129e4720a9992e40b3ba708fb684e4ade4501346
levi.goh <levi.goh@kakaoenterprise.com>
2022-07-06 15:38:35 +0900

    [server] nestjs 게시판 불러오기, 등록하기 메소드 추가

--- server-nest/src/app.module.ts
+++ server-nest/src/app.module.ts
@@ -2,14 +2,18 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import emailConfig from 'config/emailConfig';
 import { validationSchema } from 'config/validationSchema';
+import { BoardsModule } from './boards/boards.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    envFilePath: [`${__dirname}/config/env/.${process.env.NODE_ENV}.env`],
-    load: [emailConfig],
-    isGlobal: true,
-    validationSchema,
-  })],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`${__dirname}/config/env/.${process.env.NODE_ENV}.env`],
+      load: [emailConfig],
+      isGlobal: true,
+      validationSchema,
+    }),
+    BoardsModule,
+  ],
   controllers: [],
   providers: [],
 })
